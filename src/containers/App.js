c4eac219e78ab7def156905005fc2a2719f87544
Min <barrs.mindy@gmail.com>
2019-01-13 15:18:29 -0500

    Adding a Check for if there are no robots

--- src/containers/App.js
+++ src/containers/App.js
@@ -35,15 +35,19 @@ class App extends React.Component { //<--- Smart Component
             return robots.name.toLowerCase().includes(this.state.searchFeild.toLowerCase());
         });
 
-        return(
-            <div className="tc">
-                <h1 className="f2">RoboFriends</h1>
+        if(this.state.robots.length === 0){
+            return <h1 className="f2 tc">Loading....</h1>
+        }else{
+            return(
+                <div className="tc">
+                    <h1 className="f2">RoboFriends</h1>
 
-                <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={this.onSearchChange}/>
 
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
