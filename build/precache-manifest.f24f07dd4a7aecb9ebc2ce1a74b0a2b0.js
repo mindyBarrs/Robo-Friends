0d01f46cf345ec6672242cc58ef30090b8b6c2fb
Min <barrs.mindy@gmail.com>
2019-01-14 14:22:32 -0500

    Changed the gigitgnore file to added the build file

---
+++ build/precache-manifest.f24f07dd4a7aecb9ebc2ce1a74b0a2b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "f8ae75e734db90e31acf",
+    "url": "/robo-friends/static/js/main.f8ae75e7.chunk.js"
+  },
+  {
+    "revision": "5e9a133c57c4c6ca14c6",
+    "url": "/robo-friends/static/js/1.5e9a133c.chunk.js"
+  },
+  {
+    "revision": "f8ae75e734db90e31acf",
+    "url": "/robo-friends/static/css/main.f419dfa8.chunk.css"
+  },
+  {
+    "revision": "5e9a133c57c4c6ca14c6",
+    "url": "/robo-friends/static/css/1.47b48217.chunk.css"
+  },
+  {
+    "revision": "8c7c2183079ad825fbad24067d2e212e",
+    "url": "/robo-friends/index.html"
+  }
+];
